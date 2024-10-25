905cb491dfa3c76c12cba5971e052a2514664818
Emin Talha Çelik <86913426+eminbaba7231@users.noreply.github.com>
2024-10-25 15:30:00 +0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+void sayHello(const char *name);
+
+int main(){
+    char name[20];
+
+  printf("Enter your name: ");
+  scanf("%s", name);
+  
+  sayHello(name);
+   return 0;
+}
+ void sayHello(const char *name){
+   printf("Hello, %s!\n",name);
+ }
