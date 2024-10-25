e5806c3ba6d2a0ee88dcf43a232c2c454f235089
Emin Talha Çelik <86913426+eminbaba7231@users.noreply.github.com>
2024-10-25 14:21:54 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int age;
+  
+    printf("Please enter your age: ");
+    scanf("%d", &age);
+
+    for (int i = 0; i < age; i++) {
+        printf("Happy Birthday!\n");
+    }
+
+    return 0;
+}
